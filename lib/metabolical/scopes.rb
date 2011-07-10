d55d19a083f1f5b9d2d12606a6eb239d2cb67cfb
Diego Scataglini <diego@junivi.com>
2011-07-09 23:37:44 -0400

    sped up  without_meta query

--- lib/metabolical/scopes.rb
+++ lib/metabolical/scopes.rb
@@ -6,9 +6,7 @@ module Metabolical
         klass.send method, :with_metas, :includes => [:metas]
         klass.send method, :with_meta_data, lambda{|k, v| {:joins => [:metas], :conditions => {"meta_data.key" => k, "meta_data.data" => v.to_yaml}}}
         klass.send method, :without_meta, lambda{|k| 
-          {:joins => [:metas], 
-           :conditions => ["meta_data.metabolized_id not in (?)", 
-           klass.send(:with_meta, k).all(:select => 'distinct(metabolized_id)').map(&:metabolized_id)]}
+          {:joins => "left join meta_data md on md.metabolized_id = #{klass.table_name}.#{klass.primary_key} and md.metabolized_type = '#{klass.to_s}' and md.key = '#{k}'", :conditions => "md.metabolized_id is null"}
         }        
     end
     
@@ -18,6 +16,11 @@ module Metabolical
           scope :with_meta, lambda{|k| joins([:meta]).where({"meta_data.key" => k}) }
           scope :with_metas, includes(:meta)
           scope :with_meta_data, lambda{|k, v| joins(:meta).where("meta_data.key" => k, "meta_data.data" => v.to_yaml)}
+          # TODO: find a better way to define this
+          # Arel provides a way to specify left joins by using
+          # join(left).on(... syntax
+          scope :without_meta, lambda{|k| {:joins => "left join meta_data md on md.metabolized_id = #{klass.table_name}.#{klass.primary_key} and md.metabolized_type = '#{klass.to_s}' and md.key = '#{k}'", :conditions => "md.metabolized_id is null"}
+          }
         end
       end
     end
